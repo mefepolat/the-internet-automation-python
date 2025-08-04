11835aeb5b6c3a2f244a48c044fd29cbdc02a11f
Efe Polat <64933953+mefepolat@users.noreply.github.com>
2025-08-03 22:41:25 -0400

    some other tests

--- the_internet_automation/conftest.py
+++ the_internet_automation/conftest.py
@@ -4,17 +4,21 @@ from selenium.webdriver.chrome.options import Options
 import sys
 import os
 
+from selenium.webdriver.chrome.service import Service
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
-    # options.add_argument("--headless")  # for headless mode
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")  # for headless mode
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
+    options.binary_location = "/usr/bin/chromium"
 
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(10)
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+
 
     yield driver
     driver.quit()
